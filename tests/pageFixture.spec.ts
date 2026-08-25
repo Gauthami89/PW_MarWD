cf44d098c5f45ff6e05696455570ebe50f085659
Gauthami89 <gauthami.vn@testleaf.com>
2026-08-25 16:47:14 +0530

    Update pageFixture.spec.ts

--- tests/pageFixture.spec.ts
+++ tests/pageFixture.spec.ts
@@ -8,7 +8,7 @@ import {test} from "@playwright/test"
 test("page fixture",async ({page}) => {
 
 await page.goto("https://www.google.com/")
-
+//print the title of the page
 console.log(await page.title())
 console.log( page.url())
    
